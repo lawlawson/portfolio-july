97bb9771e069f89cfb67620ac67394648e620e42
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-07-09 20:30:12 +0100

    Started on Navbar component

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,8 +1,19 @@
+import Link from 'next/link';
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+      <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+      <div>
+        <ul>
+          <Link href='/'>
+            <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+          </Link>
+        </ul>
+      </div>
+      </div>
+    </div>
   )
 }
 
