be9ec96daab40cea55984f401e56ed37497c4c3e
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-07-13 23:26:33 +0100

    Added styling for background

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -2,9 +2,16 @@ import Link from 'next/link';
 import React, { useState} from 'react'
 import { AiOutlineClose, AiOutlineGithub, AiOutlineMenu, AiOutlineTwitter } from 'react-icons/ai';
 import { FaLinkedinIn } from 'react-icons/fa';
+import { ImBlog } from 'react-icons/im';
 
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(true);
+  }
+
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
@@ -27,18 +34,18 @@ const Navbar = () => {
             <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
           </Link>
         </ul>
-        <div className='md:hidden'>
+        <div onClick={handleNav} className='md:hidden'>
           <AiOutlineMenu size={25}/>
         </div>
       </div>
     </div>
 
-    <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+    <div className={nav ? 'fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
       <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
         <div>
           <div className='flex w-full items-center justify-between'>
           <Link href='/'>Lawrence Narh-Lawson</Link>
-          <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+          <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
             <AiOutlineClose />
           </div>
           </div>
@@ -64,14 +71,17 @@ const Navbar = () => {
           <div className='pt-40'>
             <p className='uppercase tracking-widest text-[#5651e5]'>Find me on</p>
             <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
-              <div className='rounded-full shadow-lg shadow-gray-400 p3 cursor-pointer hover:scale-105 ease-in duration-300'>
+              <div className='cursor-pointer hover:scale-105 ease-in duration-300'>
                 <FaLinkedinIn />
               </div>
-              <div className='rounded-full shadow-lg shadow-gray-400 p3 cursor-pointer hover:scale-105 ease-in duration-300'>
+              <div className='cursor-pointer hover:scale-105 ease-in duration-300'>
                 <AiOutlineTwitter />
               </div>
-              <div className='rounded-full shadow-lg shadow-gray-400 p3 cursor-pointer hover:scale-105 ease-in duration-300'>
+              <div className='cursor-pointer hover:scale-105 ease-in duration-300'>
                 <AiOutlineGithub />
+              </div>
+              <div className='cursor-pointer hover:scale-105 ease-in duration-300'>
+                <ImBlog />
               </div>
             </div>
           </div>
