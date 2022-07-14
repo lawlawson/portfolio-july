311f169472f4085e359fc1d52dbeab5ed773b908
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-07-14 20:56:29 +0100

    Created Main component

---
+++ pages/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p2 flex justify-center items-center">
+        <div>
+          <h1>Welcome, I&apos;m <span className="text-[#5e5]">Lawrence</span></h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
