f7d5cc5f782c46ff12201511c8bd4a08f41027cb
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-07-20 23:30:08 +0100

    Added initial About Me section

---
+++ pages/components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full md:h-screen p-2 flex items-center py-16'>
+      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+        <div className='col-span-2'>
+        <p>About</p>
+        <h2>Who I Am</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsa aliquid quae repellat sit fugiat soluta, sunt cum eaque atque ullam laudantium perferendis, qui porro quis omnis quaerat deleniti, asperiores sint laboriosam fuga eos vitae? Magnam quos, suscipit error, quas, ipsam illum placeat minus quibusdam ipsum libero magni repudiandae repellat!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsa aliquid quae repellat sit fugiat soluta, sunt cum eaque atque ullam laudantium perferendis, qui porro quis omnis quaerat deleniti, asperiores sint laboriosam fuga eos vitae? Magnam quos, suscipit error, quas, ipsam illum placeat minus quibusdam ipsum libero magni repudiandae repellat!</p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default About
