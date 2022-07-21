534c50e40dd3a869d5047945f5e0d1eb5f8acfc3
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-07-21 23:21:30 +0100

    About section styling

--- pages/components/About.jsx
+++ pages/components/About.jsx
@@ -5,10 +5,10 @@ const About = () => {
     <div className='w-full md:h-screen p-2 flex items-center py-16'>
       <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
         <div className='col-span-2'>
-        <p>About</p>
-        <h2>Who I Am</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsa aliquid quae repellat sit fugiat soluta, sunt cum eaque atque ullam laudantium perferendis, qui porro quis omnis quaerat deleniti, asperiores sint laboriosam fuga eos vitae? Magnam quos, suscipit error, quas, ipsam illum placeat minus quibusdam ipsum libero magni repudiandae repellat!</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsa aliquid quae repellat sit fugiat soluta, sunt cum eaque atque ullam laudantium perferendis, qui porro quis omnis quaerat deleniti, asperiores sint laboriosam fuga eos vitae? Magnam quos, suscipit error, quas, ipsam illum placeat minus quibusdam ipsum libero magni repudiandae repellat!</p>
+        <p className='uppercase text-xl tracking-widest text-[#901a1a]'>About</p>
+        <h2 className='py-4'>Who I Am</h2>
+        <p className='py-2 text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsa aliquid quae repellat sit fugiat soluta, sunt cum eaque atque ullam laudantium perferendis, qui porro quis omnis quaerat deleniti, asperiores sint laboriosam fuga eos vitae? Magnam quos, suscipit error, quas, ipsam illum placeat minus quibusdam ipsum libero magni repudiandae repellat!</p>
+        <p className='py-2 text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ipsa aliquid quae repellat sit fugiat soluta, sunt cum eaque atque ullam laudantium perferendis, qui porro quis omnis quaerat deleniti, asperiores sint laboriosam fuga eos vitae? Magnam quos, suscipit error, quas, ipsam illum placeat minus quibusdam ipsum libero magni repudiandae repellat!</p>
         </div>
         <div></div>
       </div>
