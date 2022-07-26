3f8e74f02ef83f87f4c804281ab02fc5f8d22600
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-07-26 23:33:20 +0100

    Styled one project

--- pages/components/Projects.jsx
+++ pages/components/Projects.jsx
@@ -14,10 +14,10 @@ const Projects = () => {
           <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#901a1a] to-[#901]'>
             <Image className='rounded-xl group-hover:opacity-10' src={previewImg} alt='/'/>
             <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-            <h3>Markdown Previewer</h3>
-            <p>React.js</p>
+            <h3 className='text-2xl text-white tracking-wider text-center'>Markdown Previewer</h3>
+            <p className='pb-4 pt-2 text-white text-center'>React.js</p>
             <Link href='/'>
-              <p>More info</p>
+              <p className='text-center py-3 rounded-lg bg-white text-gray-600 font-bold text-lg cursor-pointer'>More info</p>
             </Link>
           </div>
           </div>
