14a7822aabd44cbf2c46eb598129131380a9d3a9
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-07-26 23:37:03 +0100

    Added Project Item component

--- pages/components/Projects.jsx
+++ pages/components/Projects.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link';
 import previewImg  from '../../public/assets/projects/previewImg.png'
+import ProjectItem from './ProjectItem';
 
 const Projects = () => {
   return (
@@ -11,16 +12,9 @@ const Projects = () => {
         </p>
         <h2 className='py-4'>What I&apos;ve Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
-          <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#901a1a] to-[#901]'>
-            <Image className='rounded-xl group-hover:opacity-10' src={previewImg} alt='/'/>
-            <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-            <h3 className='text-2xl text-white tracking-wider text-center'>Markdown Previewer</h3>
-            <p className='pb-4 pt-2 text-white text-center'>React.js</p>
-            <Link href='/'>
-              <p className='text-center py-3 rounded-lg bg-white text-gray-600 font-bold text-lg cursor-pointer'>More info</p>
-            </Link>
-          </div>
-          </div>
+
+        <ProjectItem />
+
         </div>
       </div>
     </div>
