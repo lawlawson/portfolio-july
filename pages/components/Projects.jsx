5211080a397ecba36a4d6cf38e07242e01b91bb8
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-07-26 23:26:21 +0100

    Fixed Image issue

--- pages/components/Projects.jsx
+++ pages/components/Projects.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link';
-import { css_logo as previewImg } from '../../public/assets/skills/css_logo.png'
+import previewImg  from '../../public/assets/projects/previewImg.png'
 
 const Projects = () => {
   return (
@@ -12,7 +12,7 @@ const Projects = () => {
         <h2 className='py-4'>What I&apos;ve Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
           <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#901a1a] to-[#901]'>
-            <Image src={previewImg} rounded-xl group-hover:opacity-10 alt='/'/>
+            <Image className='rounded-xl group-hover:opacity-10' src={previewImg} alt='/'/>
             <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
             <h3>Markdown Previewer</h3>
             <p>React.js</p>
