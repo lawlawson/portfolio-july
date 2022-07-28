9a80035cbf9552ab83378701d772bfde0f3c2450
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-07-28 23:40:28 +0100

    Added basic heading to contact section

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='w-full lg:h-screen'>
+      <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
+        <p className='text-xl tracking-widest uppercase text-[#901a1a]'>Contact</p>
+        <h2 className='py-4'>Get In Touch</h2>
+      </div>
+    </div>
   )
 }
 
