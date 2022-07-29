40202437eb79da7ece88f15c48cea1cdbfeb560d
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-07-30 00:14:06 +0100

    Added contact text

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -6,6 +6,19 @@ const Contact = () => {
       <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
         <p className='text-xl tracking-widest uppercase text-[#901a1a]'>Contact</p>
         <h2 className='py-4'>Get In Touch</h2>
+        <div className='grid lg:grid-cols-5 gap-8'>
+          <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
+            <div className='lg:p-4 h-full'>
+              <div>
+                {/* <img className='' src='/' alt='/'/> */}
+              </div>
+              <div>
+                <h2 className='py-2'>Name here</h2>
+                <p>Front-End Developer</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
