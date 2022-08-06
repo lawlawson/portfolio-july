b3ef890c431fa9e2797ecbf429e941e68f57a119
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-08-06 19:26:43 +0100

    Added header to project page

--- pages/previewer.jsx
+++ pages/previewer.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Image from 'next/image';
+import previewImg from '../public/assets/projects/previewImg.png';
 
 const Previewer = () => {
-  return <div>Previewer</div>;
+  return (
+    <div className='w-full'>
+      <div className='w-screen h-[30vh] lg:h-[40vh] relative'>
+        <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10' />
+        <Image
+          className='absolute z-1'
+          layout='fill'
+          objectFit='cover'
+          src={previewImg}
+          alt='/'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Previewer;
