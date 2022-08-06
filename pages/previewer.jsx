a76b2d092d1ddcd2d6266d7942e37636ade4b3e0
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-08-06 20:04:38 +0100

    Styling of header text

--- pages/previewer.jsx
+++ pages/previewer.jsx
@@ -14,6 +14,10 @@ const Previewer = () => {
           src={previewImg}
           alt='/'
         />
+        <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
+          <h2 className='py-2'>Markdown Previewer</h2>
+          <h3>JavaScript / React</h3>
+        </div>
       </div>
     </div>
   );
