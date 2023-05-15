a080e5443f2c216ccd8794f97327950a4ef31c9b
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-05-15 21:03:52 +0100

    Removed comments

--- pages/components/Skills.jsx
+++ pages/components/Skills.jsx
@@ -84,36 +84,6 @@ const Skills = () => {
               </div>
             </div>
           </div>
-          {/* <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-              <div>
-                <Image
-                  alt=''
-                  src='/../public/assets/skills/nextjs-icon.png'
-                  width='64px'
-                  height='64px'
-                />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>Next JS</h3>
-              </div>
-            </div>
-          </div> */}
-          {/* <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-              <div>
-                <Image
-                  alt=''
-                  src='/../public/assets/skills/Git-Icon.png'
-                  width='64px'
-                  height='64px'
-                />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>Git</h3>
-              </div>
-            </div>
-          </div> */}
         </div>
       </div>
     </div>
