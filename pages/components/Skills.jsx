77b12b23045a4a56d5de4e88772cd4c896ccc848
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-06-08 18:00:49 +0100

    Updated skills images

--- pages/components/Skills.jsx
+++ pages/components/Skills.jsx
@@ -1,6 +1,12 @@
 import Image from 'next/image';
 import React from 'react';
 import htmlLogo from '/public/assets/skills/html_logo.png';
+import cssLogo from '/public/assets/skills/css_logo.png';
+import javaScriptLogo from '/public/assets/skills/javascript_logo.png';
+import reactLogo from '/public/assets/skills/react_logo.png';
+import typescriptLogo from '/public/assets/skills/ts_logo.png';
+import nextjsLogo from '/public/assets/skills/nextjs_logo.png';
+import gitLogo from '/public/assets/skills/git_logo.png';
 
 const Skills = () => {
   return (
@@ -23,12 +29,7 @@ const Skills = () => {
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div>
-                <Image
-                  alt=''
-                  src='/./public/assets/skills/css_logo.png'
-                  width='64px'
-                  height='64px'
-                />
+                <Image alt='' src={cssLogo} width='64px' height='64px' />
               </div>
               <div className='flex flex-col items-center justify-center'>
                 <p className='uppercase text-xl'>css</p>
@@ -38,12 +39,7 @@ const Skills = () => {
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div>
-                <Image
-                  alt=''
-                  src='/public/assets/skills/javascript_logo.png/'
-                  width='64px'
-                  height='64px'
-                />
+                <Image alt='' src={javaScriptLogo} width='64px' height='64px' />
               </div>
               <div className='flex flex-col items-center justify-center'>
                 <p className='text-xl'>JavaScript</p>
@@ -53,12 +49,7 @@ const Skills = () => {
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div>
-                <Image
-                  alt=''
-                  src='/../public/assets/skills/react_logo.png/'
-                  width='64px'
-                  height='64px'
-                />
+                <Image alt='' src={reactLogo} width='64px' height='64px' />
               </div>
               <div className='flex flex-col items-center justify-center'>
                 <p className='text-xl'>React</p>
@@ -68,15 +59,30 @@ const Skills = () => {
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
             <div className='grid grid-cols-2 gap-4 justify-center items-center'>
               <div>
-                <Image
-                  alt=''
-                  src='/../public/assets/skills/TS-logo.png'
-                  width='64px'
-                  height='64px'
-                />
+                <Image alt='' src={typescriptLogo} width='64px' height='64px' />
               </div>
               <div className='flex flex-col items-center justify-center'>
                 <p className='text-xl'>TypeScript</p>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div>
+                <Image alt='' src={nextjsLogo} width='64px' height='64px' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <p className='text-xl'>Next.js</p>
+              </div>
+            </div>
+          </div>
+          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
+              <div>
+                <Image alt='' src={gitLogo} width='64px' height='64px' />
+              </div>
+              <div className='flex flex-col items-center justify-center'>
+                <p className='text-xl'>Git</p>
               </div>
             </div>
           </div>
