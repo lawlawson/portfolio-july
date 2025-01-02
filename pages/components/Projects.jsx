4dc47d21b01eeacb713b6a9af6705067fb7dfcbe
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2025-01-02 23:10:43 +0000

    updated project cards

--- pages/components/Projects.jsx
+++ pages/components/Projects.jsx
@@ -23,6 +23,7 @@ const Projects = () => {
         'Landing page project is fully responsive and features a carousel using Swiper. I created this project primarily using Tailwind CSS. It is fully responsive and has some visual changes based on screen/device size.',
       imageUrl: manage,
       url: 'https://celebrated-biscochitos-f95934.netlify.app/',
+      code: 'https://github.com/lawlawson/manage-landing-page',
       tools: 'HTML & Tailwind CSS',
     },
   ];
@@ -35,11 +36,7 @@ const Projects = () => {
         </p>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-14'>
           {projects.map((project, index) => (
-            <div
-              key={index}
-              href={project.url}
-              target='_blank'
-              rel='noopener noreferrer'>
+            <div key={index}>
               <div key={index} className='bg-white rounded-lg shadow-md p-4'>
                 <Image
                   src={project.imageUrl}
@@ -56,12 +53,22 @@ const Projects = () => {
                   Tech stack: {project.tools}
                 </p>
                 <div className='flex justify-left'>
-                  <button className='text-white py-2 px-4 mt-4 mr-2'>
-                    Project
-                  </button>
-                  <button className='text-white py-2 px-4 mt-4 ml-2'>
-                    Code
-                  </button>
+                  <a
+                    href={project.url}
+                    target='_blank'
+                    rel='noopener noreferrer'>
+                    <button
+                      href='{project.url}'
+                      className='py-2 px-4 mt-4 mr-2'>
+                      Project
+                    </button>
+                  </a>
+                  <a
+                    href={project.code}
+                    target='_blank'
+                    rel='noopener noreferrer'>
+                    <button className='py-2 px-4 mt-4 ml-2'>Code</button>
+                  </a>
                 </div>
               </div>
             </div>
