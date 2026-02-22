3abf27938c692083caf9e74364000e4a11ce4844
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2026-02-22 19:22:31 +0000

    updated about me backgorund

--- pages/components/About.jsx
+++ pages/components/About.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
+    <div
+      id='about'
+      className='w-full md:h-screen p-2 flex items-center py-16 bg-white text-gray-800'>
       <div className='max-w-[1240px] m-auto md:grid grid-cols-1 gap-8'>
         <div className='col-span-2'>
           <p className='uppercase text-3xl tracking-widest text-[#901a1a] text-center'>
